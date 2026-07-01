45336a22f6ee4ecbcd8e2cd179780249e30796f3
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2026-07-02 01:33:25 +0200

    Update

--- master-dev/complete-intro-to-web-development/js/events-and-listeners/script.js
+++ master-dev/complete-intro-to-web-development/js/events-and-listeners/script.js
@@ -8,3 +8,15 @@ const paragraph = document.querySelector(".p-to-copy-to");
 input.addEventListener("keyup", function () {
   paragraph.innerText = input.value;
 });
+
+const input2 = document.querySelector(".color-input");
+const paragraph2 = document.querySelector(".color-box");
+input2.addEventListener("change", function () {
+  paragraph2.style.backgroundColor = input2.value;
+});
+
+document
+  .querySelector(".button-container")
+  .addEventListener("click", function (event) {
+    alert(`You clicked on button ${event.target.innerText}`);
+  });
