4a8183bdf66acad8ede2e1c27144a59c79bc7e0c
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2026-07-23 01:27:27 +0200

    Update

--- master-dev/complete-intro-to-web-development/wordle/script.js
+++ master-dev/complete-intro-to-web-development/wordle/script.js
@@ -2,13 +2,48 @@
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
 const ANSWER_LENGTH = 5;
+const ROUNDS = 6;
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle("hidden", !isLoading);
+}
+
+function makeMap(array) {
+  const obj = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+
+  return obj;
+}
+
 // Ensuring the typing works
 async function init() {
+  let isLoading = true;
+
+  // Grabbing the word of the day
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  // Another approach called "destructuring": const { word } = await res.json();
+  const wordParts = word.split("");
+  const map = makeMap(wordParts);
+  let done = false;
+
+  setLoading(false);
+  isLoading = false;
+
   let currentGuess = "";
   let currentRow = 0;
 
@@ -32,7 +67,36 @@ async function init() {
       return;
     }
 
+    const guessParts = currentGuess.split("");
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      // Mark as correct
+      if (guessParts[i] === wordParts[i]) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        // Do nothing
+      } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+      } else {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
+
     currentRow++;
+
+    if (currentGuess === word) {
+      alert("You have won!");
+      done = true;
+    } else if (currentRow === ROUNDS) {
+      alert(`You lost! The word was ${word}`);
+      done = true;
+    }
+
     currentGuess = "";
   }
 
@@ -45,6 +109,11 @@ async function init() {
 
   // Now we can do await wherever we want to here
   document.addEventListener("keydown", function handleKeypress(event) {
+    if (done || isLoading) {
+      // Do nothing
+      return;
+    }
+
     const action = event.key;
 
     if (action === "Enter") {
